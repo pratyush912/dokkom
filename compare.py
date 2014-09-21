2f1988793a86004d71db4fa50334a4ee1fe6e3a0
pratyush912 <pverma@agoranalytics.com>
2014-09-21 23:58:10 +0530

    validation added for input files

--- compare.py
+++ compare.py
@@ -1,18 +1,27 @@
 import argparse
 import logging
 import constants
+import os
+import stemming
 
 #logging configuration
 logging.basicConfig(filename='app.log',level=constants.LOG_LEVEL)
 
 #adding argument parser
-parser = argparse.ArgumentParser(description='Pass an input files')
+parser = argparse.ArgumentParser(description='Pass input files')
 parser.add_argument('-ff','--first-file', help='First Input file', required=True)
 parser.add_argument('-fs','--second-file', help='Second Input file', required=True)
 
 args= parser.parse_args() #parsing arguments
 first_input_file = args.first_file
 second_input_file = args.second_file
+
+if os.path.isfile(first_input_file) and os.path.isfile(second_input_file):
+  logging.debug('Valid first file')
+  tokens = stemming.readfile(first_input_file)
+  print tokens
+else:
+  logging.error('Invalid input file..')  
 
 logging.debug('First Input File:'+first_input_file)
 logging.debug('Second Input File:'+second_input_file)
--- stemming.py
+++ stemming.py
@@ -4,7 +4,7 @@ import os
 import re
 import argparse
 
-def readfile():
+def readfile(input_file):
 
   ins = open(input_file,'r')
   array = []
@@ -14,15 +14,18 @@ def readfile():
 
   stemmer = PorterStemmer()
 
+  stemmed_tokens = []
+
   for line in array:
     tokens = nltk.word_tokenize(line)
     for token in tokens:
-      print stemmer.stem(token)
-    print '----------------------------------------------------------' 
+      stemmed_tokens.append(stemmer.stem(token))
+      #print stemmer.stem(token)
+    #print '----------------------------------------------------------' 
+  return stemmed_tokens
 
-parser = argparse.ArgumentParser(description='Pass an input file')
-parser.add_argument('-i','--input-file', help='Input file', required=True)
-args= parser.parse_args()
-input_file = args.input_file
-readfile()    
+#parser = argparse.ArgumentParser(description='Pass an input file')
+#parser.add_argument('-i','--input-file', help='Input file', required=True)
+#args= parser.parse_args()
+#input_file = args.input_file
 
